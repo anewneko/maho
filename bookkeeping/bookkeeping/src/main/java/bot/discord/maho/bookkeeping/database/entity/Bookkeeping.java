ea751071bca2b75a50b736b92960eeb276a61489
anewneko <appay9563@gmail.com>
2023-08-27 11:34:37 +0800

    set Entity
    設定資料庫實體
    設定關聯欄位

---
+++ bookkeeping/bookkeeping/src/main/java/bot/discord/maho/bookkeeping/database/entity/Bookkeeping.java
@@ -0,0 +1,67 @@
+package bot.discord.maho.bookkeeping.database.entity;
+
+import java.sql.Timestamp;
+
+import bot.discord.maho.bookkeeping.core.pojo.CorePojo;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "bookkeeping", schema = "bookkeeping")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Bookkeeping extends CorePojo{
+	
+	
+	private static final long serialVersionUID = -9077990332142388724L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer bookkeepingId;
+	private Timestamp createTime;
+	private String  item;
+	private Integer  price;
+	private String dicordId;
+	
+	
+	public Integer getBookkeepingId() {
+		return bookkeepingId;
+	}
+	public void setBookkeepingId(Integer bookkeepingId) {
+		this.bookkeepingId = bookkeepingId;
+	}
+	public Timestamp getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Timestamp createTime) {
+		this.createTime = createTime;
+	}
+	public String getItem() {
+		return item;
+	}
+	public void setItem(String item) {
+		this.item = item;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	public String getDicordId() {
+		return dicordId;
+	}
+	public void setDicordId(String dicordId) {
+		this.dicordId = dicordId;
+	}
+	
+	
+}
---
+++ bookkeeping/bookkeeping/src/main/java/bot/discord/maho/bookkeeping/database/entity/User.java
@@ -0,0 +1,35 @@
+package bot.discord.maho.bookkeeping.database.entity;
+
+import java.util.List;
+
+import bot.discord.maho.bookkeeping.core.pojo.CorePojo;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity
+@Table(name = "User", schema = "bookkeeping")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends CorePojo {
+	private static final long serialVersionUID = -8565828708187381759L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer Id;
+	private String account;
+	private String password;
+	private String discordId;
+	@OneToMany
+	@JoinColumn(name = "discord_id",referencedColumnName = "discord_id")
+	private List<String> discordIds;
+}
