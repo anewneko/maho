dc760084a752966b6f02fcc5d36536e3ff289e7d
New Cat <appay9563@gmail.com>
2024-06-17 10:24:56 +0800

    修改Banner設定 加入postgre

--- src/main/java/bot/discord/maho/BookkeepingApplication.java
+++ src/main/java/bot/discord/maho/BookkeepingApplication.java
@@ -1,10 +1,7 @@
 package bot.discord.maho;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -16,21 +13,6 @@ public class BookkeepingApplication {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		SpringApplication app = new SpringApplication(BookkeepingApplication.class);
-		
-		try (var in = BookkeepingApplication.class.getResourceAsStream("/banner.txt");
-			 var reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-			 var bannerReader = new BufferedReader(reader)) {
-			var banner = new StringBuilder();
-			String line;
-			while ((line = bannerReader.readLine()) != null) 
-				banner.append(line).append('\n');
-			
-			app.setBanner((environment, sourceClass, out) -> 
-						   out.print(banner.append(
-								   String.format("				\u001B[32m マホロボ : \u001B[0m version %s\n", 
-										   environment.getProperty("discord.bot.version")))));
-		}
-        app.run(args);
+			app.run(args);
 	}
-
 }
