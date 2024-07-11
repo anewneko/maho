e7b5c11957ba50b5f4b8960b291086621f21781f
chiahusan.li <chiahusan.li@81756586.corp-partner.google.com>
2024-07-11 17:52:32 +0800

    unit test token verify

--- src/test/java/bot/discord/maho/bookkeeping/MahoRobotUnitTest.java
+++ src/test/java/bot/discord/maho/bookkeeping/MahoRobotUnitTest.java
@@ -5,6 +5,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
+import java.util.UUID;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,18 +17,27 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import bot.discord.maho.BookkeepingApplication;
+import bot.discord.maho.database.Entity.LoginDetail;
 import bot.discord.maho.database.Entity.Member;
+import bot.discord.maho.security.Model.User4Jwt;
+import bot.discord.maho.security.Service.JwtService;
 
 @AutoConfigureMockMvc
 @SpringBootTest(classes = BookkeepingApplication.class)
 public abstract class MahoRobotUnitTest {
 	@Autowired protected MockMvc mockMvc;
 	@Autowired protected ObjectMapper objectMapper;
-	protected Member member = new Member();
+	@Autowired private JwtService jwtService;
+	protected LoginDetail login = LoginDetail.getCurrent();
 	
 	@BeforeEach()
 	private void init() {
-		
+		login.setIsUsed(true);
+		login.setIsVerify(true);
+		var member = new Member();
+		member.setId(UUID.randomUUID());
+		member.setIsManager(false);
+		login.setMember(member);
 	}
 	
 	protected MockHttpServletRequestBuilder request(RestFunction func, String path , Object body) throws Exception {
@@ -41,10 +52,9 @@ public abstract class MahoRobotUnitTest {
 		default:
 			throw new Exception("Not support");
 		}
-		
 		return builder
 				 .contentType("application/json")
-//				 .header("Authorization", "Bearer " + jsonWebToken.generateJWT(user))
+				 .header("Authorization", "Bearer " + jwtService.generateToken(User4Jwt.of(login)))
 				 .content(objectMapper.writeValueAsString(body));
 		
 	}
@@ -68,8 +78,8 @@ public abstract class MahoRobotUnitTest {
         }
         
         return builder
-				 .contentType("application/json");
-//				 .header("Authorization", "Bearer " + jsonWebToken.generateJWT(user));
+				 .contentType("application/json")
+				 .header("Authorization", "Bearer " + jwtService.generateToken(User4Jwt.of(login)));
 	}
 
 
