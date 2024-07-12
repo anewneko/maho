aa02a8c80aa1d14f3460caad49d3ff62878aabad
New Cat <appay9563@gmail.com>
2024-07-12 16:35:52 +0800

    change to mysql

--- src/main/java/bot/discord/maho/api/Login/Manager/LoginManager.java
+++ src/main/java/bot/discord/maho/api/Login/Manager/LoginManager.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 
 import bot.discord.maho.api.Login.Exception.SpeedKeyNotFoundException;
 import bot.discord.maho.database.CrudService.Impl.LoginDetailService;
+import bot.discord.maho.security.Model.User4Jwt;
+import bot.discord.maho.security.Service.JwtService;
 import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
@@ -18,15 +20,26 @@ import net.dv8tion.jda.api.interactions.components.buttons.Button;
 public class LoginManager {
 	private final LoginDetailService spKeySvc;
 	private final JDA jda;
+	private final JwtService jwtSvc;
 	
 	public void verifyUser(UUID speedKey) {
 		var key = spKeySvc.findById(speedKey).orElseThrow(() -> SpeedKeyNotFoundException.of());
+		if(key.getIsUsed()) throw SpeedKeyNotFoundException.of("This key is already used.");
+		if(key.getExpireTime().before(new Date())) throw SpeedKeyNotFoundException.of("This key is expired.");
 		DateFormat dateFormat = DateFormat.getDateTimeInstance();
 		jda.getUserById(key.getMember().getDiscordId()).openPrivateChannel().queue((channel) -> {
             channel.sendMessage("你於 " + dateFormat.format(new Date()) + " \n嘗試使用SpeedKey登入マホロボ網頁")
             	   .addActionRow(List.of(Button.primary("speed_key_confirm:"+speedKey, "是，這是我"), Button.danger("speed_key_cancel:"+speedKey, "不是我")))
             	   .queue();
         });
+	}
+	
+	public String login(UUID speedKey) {
+		var loginDetail = spKeySvc.findById(speedKey).orElseThrow(() -> SpeedKeyNotFoundException.of());
+		if(loginDetail.getIsUsed() && loginDetail.getIsVerify())
+			return jwtSvc.generateToken(User4Jwt.of(loginDetail));
+		else
+			return "";
 	}
 
 }
