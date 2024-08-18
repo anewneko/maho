fe88aaca66f575ebf970eaea745d4c24a6d53ac1
New Cat <appay9563@gmail.com>
2024-08-18 21:43:19 +0800

    add

--- src/main/java/bot/discord/maho/core/Config/CorsConfig.java
+++ src/main/java/bot/discord/maho/core/Config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig  implements WebMvcConfigurer  {
 	@Override
     public void addCorsMappings(CorsRegistry registry)  {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3333","http://localhost:3000","https://discord.com","https://cloudflare.com")
+                .allowedOrigins(frontEndUrl,"http://localhost:3333","http://localhost:3000","https://discord.com","https://cloudflare.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .exposedHeaders("Authorization", "Header2");
     }
